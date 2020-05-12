db1a4b4f638aaba388a77adcd0647091597d5467
niklaslang <niklasjonathanlang@gmail.com>
2020-05-12 21:13:24 +0100

    filtered cells, normalized and scaled data

--- raredon_lung.R
+++ raredon_lung.R
@@ -7,7 +7,6 @@ library(reticulate)
 
 # load lung data
 lung.data.dir <- c("/home/s1987963/processed_data/raredon_lung/healthy/GSM3926545_Hum1/", "/home/s1987963/processed_data/raredon_lung/healthy/GSM3926546_Hum2/")
-
 lung.data <- Read10X(data.dir = lung.data.dir)
 
 # initialize the Seurat object
@@ -35,12 +34,36 @@ plot3 <- qplot(x =lung[["nCount_RNA"]]$nCount_RNA, fill=..count.., geom="histogr
 plot4 <- qplot(x =lung[["percent.mt"]]$percent.mt, fill=..count.., geom="histogram", binwidth = 0.1,
           xlab = "Fraction Mitochondrial Counts",
           ylab = "Frequency",
-          main = "Mitochondrial reads fraction per cell")+scale_fill_gradient(low="yellow", high="green")
-
+          main = "Mitochondrial Reads Fraction")+scale_fill_gradient(low="yellow", high="green")
 plot3 + plot4
 
 # filter cells
+# plot cut-offs
+plot5 <- plot3 + geom_vline(aes(xintercept=22000),color="black", linetype="dashed", size=.5) + geom_vline(aes(xintercept=200),color="black", linetype="dashed", size=.5)
+plot6 <- plot4 + geom_vline(aes(xintercept=5),color="black", linetype="dashed", size=.5)
+plot5 + plot6
 
+# apply cut-offs
+lung <- subset(lung, subset = nFeature_RNA > 200 & nFeature_RNA < 22000 & percent.mt < 5)
 
+# normalize count matrix
+lung <- NormalizeData(lung, normalization.method = "LogNormalize", scale.factor = 10000)
 
+# feature selection: selecting the most highly variable genes
+lung <- FindVariableFeatures(lung, selection.method = "vst", nfeatures = 2000)
 
+# 10 most highly variable genes
+top10 <- head(VariableFeatures(lung), 10)
+
+# plot variable features with and without labels
+plot7 <- VariableFeaturePlot(lung)
+plot8 <- LabelPoints(plot = plot7, points = top10, repel = TRUE)
+plot7
+plot8
+
+# scale data
+all.genes <- rownames(lung)
+lung <- ScaleData(lung, features = all.genes)
+
+# dimensionality reduction: PCA
+lung <- RunPCA(lung, features = VariableFeatures(object = lung))
