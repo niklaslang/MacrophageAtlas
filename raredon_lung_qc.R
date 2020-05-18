17869899a576d6718f04fb292f5b95b7bd5cbbe1
niklaslang <niklasjonathanlang@gmail.com>
2020-05-18 10:08:31 +0100

    QC for each patient(sample) seperately

--- raredon_lung_qc.R
+++ raredon_lung_qc.R
@@ -6,30 +6,69 @@ library(umap)
 library(reticulate)
 
 ### load lung data ###
-lung.data.dir <- c("/home/s1987963/processed_data/raredon_lung/healthy/GSM3926545_Hum1/", "/home/s1987963/processed_data/raredon_lung/healthy/GSM3926546_Hum2/")
-lung.data <- Read10X(data.dir = lung.data.dir)
+lung.data.dir <- "/home/s1987963/processed_data/raredon_lung/healthy/"
+lung.data.patients <- c("GSM3926545_Hum1","GSM3926546_Hum2", "GSM4050097_Hum3",
+                        "GSM4050099_Hum4","GSM4050101_Hum5","GSM4050103_Hum6",
+                        "GSM4050105_Hum7","GSM4050107_Hum8","GSM4050109_Hum9",
+                        "GSM4050111_Hum10","GSM4050112_Hum11","GSM4050113_Hum12",
+                        "GSM4050114_Hum13","GSM4050115_Hum14")
+lung.patient.ID <- sprintf("%02d", 1:length(lung.data.patients))
 
-# initialize the Seurat object
-lung <- CreateSeuratObject(counts = lung.data, project = "lung_raredon", min.cells = 3, min.features = 200)
+# load lung samples separately
+for(i in 1:length(lung.patient.ID)){
+  assign(paste0("lung.patient",lung.patient.ID[i],".data"), Read10X(data.dir = paste0(lung.data.dir, lung.data.patients[i])))
+}
 
-### QC ###
+# initialize Seurat objects
+for(i in 1:length(lung.patient.ID)){
+  data <- paste0("lung.patient",lung.patient.ID[i])
+  count_data <- paste0("lung.patient",lung.patient.ID[i],".data")
+  label <- paste0("patient",lung.patient.ID[i])
+  eval(parse(text=paste0(data," <- CreateSeuratObject(counts = ", count_data,", project = \"", label, "\",min.cells=3, min.features=200)")))
+  eval(parse(text=paste0("rm(", count_data, ")")))
+}
 
 ## Calculate missing metrics ##
 # compute fraction of mitochondrial gene counts
-lung[["percent.mt"]] <- PercentageFeatureSet(lung, pattern = "^MT-")
+for(i in 1:length(lung.patient.ID)){
+  data <- paste0("lung.patient",lung.patient.ID[i])
+  eval(parse(text=paste0(data,"[[\"percent.mt\"]] <- PercentageFeatureSet(", data, ", pattern = \"^MT-\")")))
+}
 
-## QC plots before filtering ##
+## merge Seurat objects
+lung.all <- merge(lung.patient01, c(lung.patient02, lung.patient03, lung.patient04,
+                                   lung.patient05, lung.patient06, lung.patient07,
+                                   lung.patient08, lung.patient09, lung.patient10,
+                                   lung.patient11, lung.patient12, lung.patient13,
+                                   lung.patient14))
+### save merged data ###
+saveRDS(lung.all, file = "/home/s1987963/MacrophageAtlas/raredon_lung_all.rds")
 
+### read data ###
+lung.all <- readRDS("/home/s1987963/MacrophageAtlas/raredon_lung_all.rds")
+
+### QC metrics ###
+## QC at patient level ##
+# unique genes per cell
+VlnPlot(lung.all, features = c("nFeature_RNA"), group.by = "orig.ident", pt.size = 0.5)
+VlnPlot(lung.all, features = c("nFeature_RNA"), group.by = "orig.ident", pt.size = 0)
+
+# unique transcripts per cell
+VlnPlot(lung.all, features = c("nCount_RNA"), group.by = "orig.ident", pt.size = 0.5)
+VlnPlot(lung.all, features = c("nCount_RNA"), group.by = "orig.ident", pt.size = 0)
+
+# mitochondrial fraction per cell
+VlnPlot(lung.all, features = c("percent.mt"), group.by = "orig.ident", pt.size = 0.5)
+VlnPlot(lung.all, features = c("percent.mt"), group.by = "orig.ident", pt.size = 0)
+
+## overall QC ##
 # violin plots
 QC.violin <- function(data){
-  violin.plots <- VlnPlot(data, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+  violin.plots <- VlnPlot(data, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3, group.by = NULL)
   return(violin.plots)
 }
-QC.violin.before <- QC.violin(lung)
+QC.violin.before <- QC.violin(lung.all)
 QC.violin.before
-
-#plot1 <- VlnPlot(lung, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
-#plot1
 
 # scatter plots
 QC.scatter <- function(data){
@@ -40,39 +79,38 @@ QC.scatter <- function(data){
   scatter.plots[[2]] <- scatter2
   return(scatter.plots)
 }
-QC.scatter.before <- QC.scatter(lung)
+QC.scatter.before <- QC.scatter(lung.all)
 QC.scatter.before[[1]] + QC.scatter.before[[2]]
 
-#plot2 <- FeatureScatter(lung, feature1 = "nCount_RNA", feature2 = "percent.mt")
-#plot3 <- FeatureScatter(lung, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
-#plot2 + plot3
-
 # histograms
-
 QC.histograms <- function(data){
   histograms <- list()
   
   # distribution of genes per cell
   hist1 <- qplot(x =data[["nFeature_RNA"]]$nFeature_RNA , fill=..count.., geom="histogram", binwidth = 100,
-                 xlab = "Genes",
+                 xlab = "Unique genes per cell",
                  ylab = "Frequency",
-                 main = "Genes per cell")+scale_fill_gradient(low="lightblue", high="darkblue")
+                 main = "Gene Count Distribution")+scale_fill_gradient(low="lightblue", high="darkblue")
   histograms[[1]] <- hist1
   
   # distribution of count depth
-  hist2 <- qplot(x =data[["nCount_RNA"]]$nCount_RNA, fill=..count.., geom="histogram", binwidth = 1000,
-                 xlab = "Count depth",
+  hist2 <- qplot(x =data[["nCount_RNA"]]$nCount_RNA, fill=..count.., geom="histogram", binwidth = 100,
+                 xlab = "Unique transcripts per cell",
                  ylab = "Frequency",
-                 main = "Count depth per cell")+scale_fill_gradient(low="orange", high="red")
+                 main = "Transcript Count Distribution")+scale_fill_gradient(low="orange", high="red")
   histograms[[2]] <- hist2
   
   # distribution of mitochondrial gene fraction
   hist3 <- qplot(x =data[["percent.mt"]]$percent.mt, fill=..count.., geom="histogram", binwidth = 0.1,
-                 xlab = "Fraction Mitochondrial Counts",
+                 xlab = "Mitochondrial fraction per cell",
                  ylab = "Frequency",
                  main = "Mitochondrial Reads Fraction")+scale_fill_gradient(low="lightgreen", high="darkgreen")
   histograms[[3]] <- hist3
   return(histograms)
 }
-QC.histograms.before <- QC.histograms(lung)
+QC.histograms.before <- QC.histograms(lung.all)
 QC.histograms.before[[1]]+QC.histograms.before[[2]]+QC.histograms.before[[3]]
+
+### conclusion ###
+
+
