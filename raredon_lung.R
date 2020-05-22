c9a277b85af5cc20348b8c8eaafd8f39d501e9f7
niklaslang <niklasjonathanlang@gmail.com>
2020-05-22 15:54:16 +0100

    included loop both batch control

--- raredon_lung.R
+++ raredon_lung.R
@@ -5,8 +5,8 @@ library(dplyr)
 library(ggplot2)
 library(patchwork)
 
-### load lung data ###
-lung <- readRDS("/home/s1987963/MacrophageAtlas/raredon_lung.rds")
+### load lung.all data ###
+lung <- readRDS("/home/s1987963/MacrophageAtlas/raredon_lung_all.rds")
 
 ### normalisation ###
 lung <- NormalizeData(lung, normalization.method = "LogNormalize", scale.factor = 10000)
@@ -99,3 +99,126 @@ FeaturePlot(lung, features = MNP.genes)
 ### save file ###
 saveRDS(lung, file = "/home/s1987963/MacrophageAtlas/raredon_lung_final.rds")
 
+#### processing lung.11 data ###
+### load data ###
+lung.11 <- readRDS("/home/s1987963/MacrophageAtlas/raredon_lung_11.rds")
+
+### normalisation ###
+lung.11 <- NormalizeData(lung.11, normalization.method = "LogNormalize", scale.factor = 10000)
+
+### feature selection ###
+# selecting the most highly variable genes
+lung.11 <- FindVariableFeatures(lung.11, selection.method = "vst", nfeatures = 2000)
+
+### scale data ###
+lung.11 <- ScaleData(lung.11, vars.to.regress = "nFeature_RNA")
+
+### dimensionality reduction: PCA ###
+lung.11 <- RunPCA(lung.11, features = VariableFeatures(object = lung.11))
+
+## explore PCA results ##
+# genes making up the first 15 PCs
+print(lung.11[["pca"]], dims = 1:15, nfeatures = 5)
+
+# plot genes making PC1 and PC2
+VizDimLoadings(lung.11, dims = 1:2, reduction = "pca")
+
+# plot PC1 against PC2
+DimPlot(lung.11, reduction = "pca")
+
+# PC heatmap
+for(i in 1:4){
+  lower_limit <- 1+(6*(i-1))
+  upper_limit <- 6*i
+  DimHeatmap(lung.11, dims = lower_limit:upper_limit, nfeatures = 20, cells = 500, balanced = TRUE)
+}
+
+## elbow plot ##
+ElbowPlot(lung.11, ndims = 50)
+
+### save data ###
+saveRDS(lung.11, file = "/home/s1987963/MacrophageAtlas/raredon_lung_11_pca.rds")
+
+### clustering ###
+## evaluate different numbers of PCs and resolutions ##
+# create batch plots
+dims <- c(5,6,9,10,11,16,19)
+for(d in dims){
+  lung.11 <- RunUMAP(lung.11, dims=1:d, seed.use=1)
+  batch.plot <- DimPlot(lung.11, reduction = "umap", group.by = "orig.ident", pt.size = 0.1)
+  eval(parse(text=paste0("UMAP_dim", d, "batch <- batch.plot")))
+}
+
+# compare clustering and batch effect
+dims <- c(5,6)
+res <- c(.3,1.4,1.5,1.6,1.7,1.8)
+#res <- seq(0.2,2.0,0.2)
+for(d in dims){
+  lung.11 <- RunUMAP(lung.11, dims=1:d, seed.use=1)
+  for (r in res) {
+    lung.11 <- FindNeighbors(lung.11, dims = 1:d)
+    lung.11 <- FindClusters(lung.11, resolution = r)
+    umap.plot <- DimPlot(lung.11, reduction = "umap", label = F)
+    batch.plot <- DimPlot(lung.11, reduction = "umap", group.by = "orig.ident")
+    eval(parse(text=paste0("UMAP_dim", d, "_res", r, " <- umap.plot + batch.plot")))
+  }
+}
+
+#### processing lung.9 data ###
+### load data ###
+lung.9 <- readRDS("/home/s1987963/MacrophageAtlas/raredon_lung_9.rds")
+
+### normalisation ###
+lung.9 <- NormalizeData(lung.9, normalization.method = "LogNormalize", scale.factor = 10000)
+
+### feature selection ###
+# selecting the most highly variable genes
+lung.9 <- FindVariableFeatures(lung.9, selection.method = "vst", nfeatures = 2000)
+
+### scale data ###
+lung.9 <- ScaleData(lung.9)
+
+### dimensionality reduction: PCA ###
+lung.9 <- RunPCA(lung.9, features = VariableFeatures(object = lung.9))
+
+## explore PCA results ##
+# genes making up the first 15 PCs
+print(lung.9[["pca"]], dims = 1:15, nfeatures = 5)
+
+# plot genes making PC1 and PC2
+VizDimLoadings(lung.9, dims = 1:2, reduction = "pca")
+
+# plot PC1 against PC2
+DimPlot(lung.9, reduction = "pca")
+
+# PC heatmap
+for(i in 1:4){
+  lower_limit <- 1+(6*(i-1))
+  upper_limit <- 6*i
+  DimHeatmap(lung.9, dims = lower_limit:upper_limit, nfeatures = 20, cells = 500, balanced = TRUE)
+}
+
+## elbow plot ##
+ElbowPlot(lung.9, ndims = 50)
+
+### save data ###
+saveRDS(lung.9, file = "/home/s1987963/MacrophageAtlas/raredon_lung_9_pca.rds")
+
+### clustering ###
+# evaluate different numbers of PCs and resolutions
+dims <- c(6,9,10,11,15,20)
+res <- seq(0.2,1.2,0.1)
+
+#
+
+# 
+for(d in dims){
+  lung.9 <- RunUMAP(lung.9, dims=1:d, seed.use=1)
+  for (r in res) {
+    lung.9 <- FindNeighbors(lung.9, dims = 1:d)
+    lung.9 <- FindClusters(lung.9, resolution = r)
+    umap.plot <- DimPlot(lung.9, reduction = "umap", label = F)
+    batch.plot <- DimPlot(lung.9, reduction = "umap", group.by = "orig.ident")
+    eval(parse(text=paste0("UMAP_dim", d, "_res", r, " <- umap.plot + batch.plot")))
+  }
+}
