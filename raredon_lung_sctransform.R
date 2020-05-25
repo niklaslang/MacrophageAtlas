992b8040f639986d285d6720d0829c4645b43779
niklaslang <niklasjonathanlang@gmail.com>
2020-05-25 21:55:25 +0100

    adjusted for lung data characteristics

--- raredon_lung_sctransform.R
+++ raredon_lung_sctransform.R
@@ -9,45 +9,45 @@ library(ggplot2)
 library(patchwork)
 
 ### load lung data ###
-lung <- readRDS("/home/s1987963/MacrophageAtlas/raredon_lung_4.rds")
+lung <- readRDS("/home/s1987963/MacrophageAtlas/raredon_lung.rds")
 
 ### split lung data ###
 lung.list <- SplitObject(lung, split.by = "patient.ID")
 
 ### perform SCtransform ###
-options(future.globals.maxSize = 4000 * 1024^2)
+options(future.globals.maxSize = 8000 * 1024^2)
 for (i in 1:length(lung.list)) {
-  lung.list[[i]] <- SCTransform(lung.list[[i]], verbose = FALSE)
+  lung.list[[i]] <- SCTransform(lung.list[[i]], verbose = TRUE)
 }
 
 ### select features for downstream integration ###
 lung.features <- SelectIntegrationFeatures(object.list = lung.list, nfeatures = 3000)
 
 ### run PrepSCTIntegration ###
-lung.list <- PrepSCTIntegration(object.list = lung.list, anchor.features = lung.features, 
-                                    verbose = FALSE)
+lung.list <- PrepSCTIntegration(object.list = lung.list, anchor.features = lung.features, verbose = TRUE)
 
 ### identify anchors ###
-lung.anchors <- FindIntegrationAnchors(object.list = lung.list, normalization.method = "SCT", 
-                                           anchor.features = lung.features, verbose = FALSE)
+lung.anchors <- FindIntegrationAnchors(object.list = lung.list, anchor.features = lung.features,
+                                       normalization.method = "SCT", k.filter = 60, verbose = TRUE)
 
 ### integrate datasets ###
-lung.sctransform <- IntegrateData(anchorset = lung.anchors, normalization.method = "SCT", 
-                                     verbose = FALSE)
+lung.sctransform <- IntegrateData(anchorset = lung.anchors, normalization.method = "SCT", verbose = TRUE)
 
 ### PCA ### 
-lung.sctransform <- RunPCA(lung.sctransform, verbose = FALSE)
+lung.sctransform <- RunPCA(lung.sctransform, verbose = TRUE)
+
 # elbow plot
 ElbowPlot(lung.sctransform, ndims = 50)
 
 ### UMAP visualisation ###
-# first 10 PCs
-lung.sctransform <- RunUMAP(lung.sctransform, dims = 1:10)
-DimPlot(lung.sctransform, group.by = "patient.ID", pt.size = 0.1)
-# first 15 PCs
-lung.sctransform <- RunUMAP(lung.sctransform, dims = 1:15)
-DimPlot(lung.sctransform, group.by = "patient.ID", pt.size = 0.1)
+dims <- c(6,8,10,12,15)
+for(d in dims){
+  lung.sctransform <- RunUMAP(lung.sctransform, dims = 1:d)
+  umap.batch.plot <- DimPlot(lung, reduction = "umap", label = F)
+  eval(parse(text=paste0("UMAP.batch_dim", d, " <- umap.batch.plot")))
+}
 
-
+### save data ###
+saveRDS(lung.sctransform, file = "/home/s1987963/MacrophageAtlas/raredon_lung_sctransform.rds")
 
 
