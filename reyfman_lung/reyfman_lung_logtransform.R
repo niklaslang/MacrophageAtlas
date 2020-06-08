1ca38d61f182ce05c086664c72b10c5cb2692076
niklaslang <niklasjonathanlang@gmail.com>
2020-06-08 13:25:12 +0100

    compared three scaling strategies

--- reyfman_lung/reyfman_lung_logtransform.R
+++ reyfman_lung/reyfman_lung_logtransform.R
@@ -9,8 +9,8 @@ library(patchwork)
 ### path variables ###
 lung.path <- "/home/s1987963/ds_group/Niklas/reyfman_lung/reyfman_lung_healthy.rds"
 # logtransform.path <- "/home/s1987963/ds_group/Niklas/reyfman_lung/logtransform/uncorrected/" # uncorrected/unadjusted
-# logtransform.path <- "/home/s1987963/ds_group/Niklas/reyfman_lung/logtransform/regress.nFeatures/" # corrected for nFeature
-logtransform.path <- "/home/s1987963/ds_group/Niklas/reyfman_lung/logtransform/regress.nCounts.mito/" # corrected for nCounts and mitochondrial fraction
+logtransform.path <- "/home/s1987963/ds_group/Niklas/reyfman_lung/logtransform/regress.nFeatures/" # corrected for nFeature
+# logtransform.path <- "/home/s1987963/ds_group/Niklas/reyfman_lung/logtransform/regress.nCounts.mito/" # corrected for nCounts and mitochondrial fraction
   
 ### read healthy lung data ###
 lung <- readRDS(lung.path)
@@ -32,8 +32,8 @@ HVGs.plot
 
 ### scale data ###
 # lung.logtransform <- ScaleData(lung) # uncorrected
-# lung.logtransform <- ScaleData(lung, vars.to.regress = "nFeature_RNA") # adjusted for nFeatures
-lung.logtransform <- ScaleData(lung, vars.to.regress = c("nCount_RNA", "percent.mt")) # adjusted for nCounts + percent.mt
+lung.logtransform <- ScaleData(lung, vars.to.regress = "nFeature_RNA") # adjusted for nFeatures
+# lung.logtransform <- ScaleData(lung, vars.to.regress = c("nCount_RNA", "percent.mt")) # adjusted for nCounts + percent.mt
 
 ### dimensionality reduction: PCA ###
 lung.logtransform <- RunPCA(lung.logtransform, features = VariableFeatures(object = lung))
@@ -44,13 +44,24 @@ png(paste0(logtransform.path,"logtransform.elbow.plot.png"), width=1000,height=6
 print(logtransform.elbow.plot)
 dev.off()
 
+### visualize batch effect - pre-selection of dimensions -  ##
+dims <- c(4,6,9,15,17)
+for(d in dims){
+  lung.logtransform <- RunUMAP(lung.logtransform, dims = 1:d, seed.use=1)
+  umap.batch.plot <- DimPlot(lung.logtransform, reduction = "umap", group.by = "patient.ID", pt.size = 0.1)
+  png(paste0(logtransform.path, "UMAP_dim", d, "batch.png"), width=1400, height=600, units="px")
+  print(umap.batch.plot)
+  dev.off()
+}
+
+### clustering ###
+## evaluate different numbers of PCs and resolutions ##
 ### clustering ###
 # evaluate different numbers of PCs and resolutions
-#dims <- c() #adjusted for nFeatures
-dims <- c(4,6,9,11,15,17,25) #adjusted for nCounts + percent.mito
+dims <- c(6,9) #adjusted for nFeatures
+# dims <- c(4,6,9,11,15,17,25) #adjusted for nCounts + percent.mito
 #dims <- c(6,8,9,11,15,20,30) # uncorrected/adjusted
 res <- seq(0.3,1.5,0.1)
-
 for(d in dims){
   lung.logtransform <- RunUMAP(lung.logtransform, dims=1:d, seed.use=1)
   for (r in res) {
@@ -68,7 +79,7 @@ for(d in dims){
 ### definite clustering ###
 ## best (preliminary) clustering ##
 # uncorrected/adjusted: 8 first PCs, resolution 0.3
-# adjusted for nFeatures: XY first PCs, resolution XY
+# adjusted for nFeatures: 6 first PCs, resolution 0.4
 # adjusted for nCounts + percent.mito: 6 first PCs, resolution 0.4
 lung.logtransform <- FindNeighbors(lung.logtransform, dims = 1:6)
 lung.logtransform <- FindClusters(lung.logtransform, resolution = 0.4)
@@ -103,8 +114,9 @@ dc.genes <- c("CD1C","XCR1", "CD86", "CCL17", "S100B", "RGS1")
 # lineage markers
 lineage.genes <- c("EPCAM", #epithelial cells
                    "CDH5", "PECAM1", "VWF", "KDR", #endothelial cells
-                   "PDGFRA", "PDGFRB", "ACTA2", "MYH11", "CD34", #mesenchymal cells
+                   "PDGFRA", "PDGFRB", "ACTA2", "MYH11",  #mesenchymal cells - maybe "CD34"?
                    "PTPRC", #immune cells
+                   "TPSB2", #mast cells - maybe "IL1RL1"?
                    "CD3D", "GZMA", #T-cells
                    "CD79A", "CD79B" #B-cells
 )
@@ -133,6 +145,6 @@ dev.off()
 # feature plot with more general lineage markers
 lineage.markers <- FeaturePlot(lung.logtransform, features = lineage.genes, pt.size = 0.2, ncol = 5) & 
   scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
-png(paste0(logtransform.path,"lineage.markers.png"), width=1800,height=1000,units="px")
+png(paste0(logtransform.path,"lineage.markers.png"), width=1800,height=1200,units="px")
 print(lineage.markers)
 dev.off()
