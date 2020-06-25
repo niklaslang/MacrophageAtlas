ef2e3b8b67139fa1180f116fbc2853405c51bd2b
niklaslang <niklasjonathanlang@gmail.com>
2020-06-25 09:08:35 +0100

    performed clustering at multiple resolution

--- reyes_blood/reyes_blood_harmony.R
+++ reyes_blood/reyes_blood_harmony.R
@@ -7,11 +7,10 @@ library(ggplot2)
 library(cowplot)
 library(RColorBrewer)
 library(patchwork)
-options(future.globals.maxSize = 2000 * 1024^2)
 
 ### path variables ###
 blood.path <- "/home/s1987963/ds_group/Niklas/reyes_blood/reyes_blood_healthy.rds"
-harmony.path <- "/home/s1987963/ds_group/Niklas/reyes_blood/harmony/uncorrected/dim12_annotation/"
+harmony.path <- "/home/s1987963/ds_group/Niklas/reyes_blood/harmony/uncorrected/"
 
 ### load blood data ###
 blood <- readRDS(blood.path)
@@ -54,71 +53,8 @@ png(paste0(harmony.path,"harmony.elbow.plot.png"), width=1000,height=600,units="
 print(harmony.elbow.plot)
 dev.off()
 
-### visualize batch effect - pre-selection of dimensions ##
-dims <- c(8,12,14,18,29,50)
-for(d in dims){
-  blood.harmony <- RunUMAP(blood.harmony, reduction = "harmony", dims = 1:d, seed.use=1)
-  umap.batch.plot <- DimPlot(blood.harmony, reduction = "umap", group.by = "patient.ID", pt.size = 0.1)
-  png(paste0(harmony.path, "UMAP_dim", d, ".batch.png"), width=1400, height=600, units="px")
-  print(umap.batch.plot)
-  dev.off()
-}
-
-## clustering ##
-#dims <- c() #adjusted for nFeatures
-# dims <- c() #adjusted for nCounts + percent.mito
-dims <- c(8,12)
-res <- seq(0.1,1.0,0.1)
-plan("multiprocess", workers = 4)
-for(d in dims){
-  blood.harmony <- RunUMAP(blood.harmony, reduction = "harmony", dims=1:d, seed.use=1)
-  
-  # plot batch effect
-  batch.plot <- DimPlot(blood.harmony, reduction = "umap", group.by = "patient.ID", pt.size = 0.1)
-
-  for (r in res) {
-    blood.harmony <- FindNeighbors(blood.harmony, reduction = "harmony", dims = 1:d)
-    blood.harmony <- FindClusters(blood.harmony, reduction = "harmony", resolution = r)
-    umap.plot <- DimPlot(blood.harmony, reduction = "umap", label = F, pt.size = 0.1)
-    
-    # create double plot
-    eval.plot <- umap.plot + batch.plot
-    png(paste0(harmony.path, "UMAP_dim", d, "_res", r, ".png"), width=1500, height=600, units="px")
-    print(eval.plot)
-    dev.off()
-  }
-}
-
-### save data ###
-saveRDS(blood.harmony, file = paste0(harmony.path, "reyes_blood_harmony.rds"))
-
-## preliminary clustering ##
-# uncorrected/adjusted: 8/12 first PCs, resolution 0.1
-# adjusted for nFeatures: XY first PCs, resolution XY
-# adjusted for nCounts + percent.mito: XY first PCs, resolution
-blood.harmony <- FindNeighbors(blood.harmony, reduction = "harmony", dims = 1:12)
-blood.harmony <- FindClusters(blood.harmony, reduction = "harmony", resolution = 0.1)
-# run UMAP
-blood.harmony <- RunUMAP(blood.harmony, reduction = "harmony", dims=1:12, seed.use=1)
-
-### save data ###
-saveRDS(blood.harmony, file = paste0(harmony.path, "reyes_blood_harmony.rds"))
-
-## explore clustering at patient level ##
-patient.clustering <- DimPlot(blood.harmony, group.by = "seurat_clusters", split.by = "patient.ID", pt.size = 0.2, ncol = 6)
-png(paste0(harmony.path,"patient.clustering.png"), width=1800,height=600,units="px")
-print(patient.clustering)
-dev.off()
-
-## QC metrics at cluster level ##
-cluster.qc.heatmap <- FeaturePlot(blood.harmony, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), pt.size = 0.2, ncol = 3) & 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
-png(paste0(harmony.path,"cluster.qc.heatmap.png"), width=1500,height=500,units="px")
-print(cluster.qc.heatmap)
-dev.off()
-
-## marker gene visualization ##
-#MNP genes
+### visualize batch effect and marker gene expression ##
+# MNP genes
 MNP.genes <- c("CD14", "FCGR3A","CSF1R", "CD68", "LYZ", 
                "CCR2", "CX3CR1", "S100A8", "MARCO", "MNDA", 
                "TIMD4", "CD163", "C1QB", "F13A1",	"MCEMP1",	
@@ -167,107 +103,179 @@ mesenchymal.genes <- c("COL1A1",	"COL3A1", "ACTA2", "MYH11",	"PDGFRA",
 # proliferating cells
 proliferation.genes <- c("MKI67",	"TOP2A")
 
-# feature plot with immune cell marker
-immunecell.markers <- FeaturePlot(blood.harmony, features = c("PTPRC"), pt.size = 0.5, ncol = 1) & 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
-png(paste0(harmony.path,"immunecell.markers.png"), width=1000,height=1000,units="px")
-print(immunecell.markers)
-dev.off()
+# selected number of PCs
+dims <- c(40)
+for(d in dims){
+  
+  # create folder
+  dir.create(paste0(harmony.path, "dim", d, "_annotation"))
+  dim.path <- paste0(harmony.path, "dim", d, "_annotation/")
+  
+  # run UMAP
+  blood.harmony <- RunUMAP(blood.harmony, reduction = "harmony", dims = 1:d, seed.use=1)
+  
+  ## batch plots ##
+  # no.1 - coloured by patients
+  batch1.plot <- DimPlot(blood.harmony, reduction = "umap", group.by = "patient.ID", pt.size = 0.01)
+  png(paste0(dim.path, "UMAP_dim", d, ".patient.batch1.png"), width=1600, height=1000, units="px")
+  print(batch1.plot)
+  dev.off()
+  
+  # no.2 - split by patients
+  batch2.plot <- DimPlot(blood.harmony, reduction = "umap", group.by = NULL, split.by = "patient.ID", pt.size = 0.01, ncol = 6)
+  png(paste0(dim.path, "UMAP_dim", d, ".patient.batch2.png"), width=1800, height=1000, units="px")
+  print(batch2.plot)
+  dev.off()
+  
+  ## QC metrics at cluster level ##
+  cluster.qc.heatmap <- FeaturePlot(blood.harmony, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), pt.size = 0.2, ncol = 3) & 
+    scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
+  png(paste0(dim.path,"UMAP_dim", d, "cluster.qc.heatmap.png"), width=1500,height=500,units="px")
+  print(cluster.qc.heatmap)
+  dev.off()
+  
+  ## gene expression heatmaps ##
+  # feature plot with immune cell marker
+  immunecell.markers <- FeaturePlot(blood.harmony, features = c("PTPRC"), pt.size = 0.5, ncol = 1) & 
+    scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
+  png(paste0(dim.path,"UMAP_dim", d, "immunecell.markers.png"), width=1000,height=1000,units="px")
+  print(immunecell.markers)
+  dev.off()
+  
+  # feature plot with MNP markers
+  MNP.markers <- FeaturePlot(blood.harmony, features = MNP.genes, pt.size = 0.5, ncol = 6) & 
+    scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
+  png(paste0(dim.path,"UMAP_dim", d, "MNP.markers.png"), width=1800,height=900,units="px")
+  print(MNP.markers)
+  dev.off()
+  
+  # feature plot with cDC 1 markers
+  cDC1.markers <- FeaturePlot(blood.harmony, features = cDC1.genes, pt.size = 0.5, ncol = 4) & 
+    scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
+  png(paste0(dim.path,"UMAP_dim", d, "cDC1.markers.png"), width=1600,height=400,units="px")
+  print(cDC1.markers)
+  dev.off()
+  
+  # feature plot with cDC 2 markers
+  cDC2.markers <- FeaturePlot(blood.harmony, features = cDC2.genes, pt.size = 0.5, ncol = 3) & 
+    scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
+  png(paste0(dim.path, "UMAP_dim", d, "cDC2.markers.png"), width=1200,height=800,units="px")
+  print(cDC2.markers)
+  dev.off()
+  
+  # feature plot with pDC markers
+  pDC.markers <- FeaturePlot(blood.harmony, features = pDC.genes, pt.size = 0.5, ncol = 4) & 
+    scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
+  png(paste0(dim.path,"UMAP_dim", d, "pDC.markers.png"), width=1600,height=400,units="px")
+  print(pDC.markers)
+  dev.off()
+  
+  # feature plot with T cell markers
+  Tcell.markers <- FeaturePlot(blood.harmony, features = Tcell.genes, pt.size = 0.5, ncol = 4) & 
+    scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
+  png(paste0(dim.path,"UMAP_dim", d, "Tcell.markers.png"), width=1600,height=800,units="px")
+  print(Tcell.markers)
+  dev.off()
+  
+  # feature plot with NK cell markers
+  NK.markers <- FeaturePlot(blood.harmony, features = NK.genes, pt.size = 0.5, ncol = 4) & 
+    scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
+  png(paste0(dim.path,"UMAP_dim", d, "NK.markers.png"), width=1600,height=800,units="px")
+  print(NK.markers)
+  dev.off()
+  
+  # feature plot with B cell markers
+  Bcell.markers <- FeaturePlot(blood.harmony, features = Bcell.genes, pt.size = 0.5, ncol = 3) & 
+    scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
+  png(paste0(dim.path,"UMAP_dim", d, "Bcell.markers.png"), width=1200,height=400,units="px")
+  print(Bcell.markers)
+  dev.off()
+  
+  # feature plot with plasma cell markers
+  plasmacell.markers <- FeaturePlot(blood.harmony, features = plasmacell.genes, pt.size = 0.5, ncol = 5) & 
+    scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
+  png(paste0(dim.path,"UMAP_dim", d, "plasmacell.markers.png"), width=1500,height=300,units="px")
+  print(plasmacell.markers)
+  dev.off()
+  
+  # feature plot with mast cell markers
+  mastcell.markers <- FeaturePlot(blood.harmony, features = mastcell.genes, pt.size = 0.5, ncol = 4) & 
+    scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
+  png(paste0(dim.path,"UMAP_dim", d,"mastcell.markers.png"), width=1600,height=400,units="px")
+  print(mastcell.markers)
+  dev.off()
+  
+  # feature plot with epithelial cell markers
+  epithelial.markers <- FeaturePlot(blood.harmony, features = epithelial.genes, pt.size = 0.5, ncol = 5) & 
+    scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
+  png(paste0(dim.path,"UMAP_dim", d,"epithelial.markers.png"), width=1500,height=400,units="px")
+  print(epithelial.markers)
+  dev.off()
+  
+  # feature plot with endothelial cell markers
+  endothelial.markers <- FeaturePlot(blood.harmony, features = endothelial.genes, pt.size = 0.5, ncol = 3) & 
+    scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
+  png(paste0(dim.path,"UMAP_dim", d,"endothelial.markers.png"), width=1200,height=1200,units="px")
+  print(endothelial.markers)
+  dev.off()
+  
+  # feature plot with mesenchymal cell markers
+  mesenchymal.markers <- FeaturePlot(blood.harmony, features = mesenchymal.genes, pt.size = 0.5, ncol = 3) & 
+    scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
+  png(paste0(dim.path,"UMAP_dim", d,"mesenchymal.markers.png"), width=1200,height=1200,units="px")
+  print(mesenchymal.markers)
+  dev.off()
+  
+  # feature plot with erythroid cell markers
+  ery.markers <- FeaturePlot(blood.harmony, features = ery.genes, pt.size = 0.5, ncol = 2) & 
+    scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
+  png(paste0(dim.path,"UMAP_dim", d,"ery.markers.png"), width=800,height=400,units="px")
+  print(ery.markers)
+  dev.off()
+  
+  # proliferating cells
+  proliferation.markers <- FeaturePlot(blood.harmony, features = proliferation.genes, pt.size = 0.5, ncol = 2) & 
+    scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
+  png(paste0(dim.path,"UMAP_dim", d,"proliferation.markers.png"), width=800,height=400,units="px")
+  print(proliferation.markers)
+  dev.off()
+  
+}
 
-# feature plot with MNP markers
-MNP.markers <- FeaturePlot(blood.harmony, features = MNP.genes, pt.size = 0.5, ncol = 6) & 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
-png(paste0(harmony.path,"MNP.markers.png"), width=1800,height=900,units="px")
-print(MNP.markers)
-dev.off()
-
-# feature plot with cDC 1 markers
-cDC1.markers <- FeaturePlot(blood.harmony, features = cDC1.genes, pt.size = 0.5, ncol = 4) & 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
-png(paste0(harmony.path,"cDC1.markers.png"), width=1600,height=400,units="px")
-print(cDC1.markers)
-dev.off()
-
-# feature plot with cDC 2 markers
-cDC2.markers <- FeaturePlot(blood.harmony, features = cDC2.genes, pt.size = 0.5, ncol = 3) & 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
-png(paste0(harmony.path,"cDC2.markers.png"), width=1200,height=800,units="px")
-print(cDC2.markers)
-dev.off()
-
-# feature plot with pDC markers
-pDC.markers <- FeaturePlot(blood.harmony, features = pDC.genes, pt.size = 0.5, ncol = 4) & 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
-png(paste0(harmony.path,"pDC.markers.png"), width=1600,height=400,units="px")
-print(pDC.markers)
-dev.off()
-
-# feature plot with T cell markers
-Tcell.markers <- FeaturePlot(blood.harmony, features = Tcell.genes, pt.size = 0.5, ncol = 4) & 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
-png(paste0(harmony.path,"Tcell.markers.png"), width=1600,height=800,units="px")
-print(Tcell.markers)
-dev.off()
-
-# feature plot with NK cell markers
-NK.markers <- FeaturePlot(blood.harmony, features = NK.genes, pt.size = 0.5, ncol = 4) & 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
-png(paste0(harmony.path,"NK.markers.png"), width=1600,height=800,units="px")
-print(NK.markers)
-dev.off()
-
-# feature plot with B cell markers
-Bcell.markers <- FeaturePlot(blood.harmony, features = Bcell.genes, pt.size = 0.5, ncol = 3) & 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
-png(paste0(harmony.path,"Bcell.markers.png"), width=1200,height=400,units="px")
-print(Bcell.markers)
-dev.off()
-
-# feature plot with plasma cell markers
-plasmacell.markers <- FeaturePlot(blood.harmony, features = plasmacell.genes, pt.size = 0.5, ncol = 5) & 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
-png(paste0(harmony.path,"plasmacell.markers.png"), width=1500,height=300,units="px")
-print(plasmacell.markers)
-dev.off()
-
-# feature plot with mast cell markers
-mastcell.markers <- FeaturePlot(blood.harmony, features = mastcell.genes, pt.size = 0.5, ncol = 4) & 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
-png(paste0(harmony.path,"mastcell.markers.png"), width=1600,height=400,units="px")
-print(mastcell.markers)
-dev.off()
-
-# feature plot with epithelial cell markers
-epithelial.markers <- FeaturePlot(blood.harmony, features = epithelial.genes, pt.size = 0.5, ncol = 5) & 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
-png(paste0(harmony.path,"epithelial.markers.png"), width=1500,height=400,units="px")
-print(epithelial.markers)
-dev.off()
-
-# feature plot with endothelial cell markers
-endothelial.markers <- FeaturePlot(blood.harmony, features = endothelial.genes, pt.size = 0.5, ncol = 3) & 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
-png(paste0(harmony.path,"endothelial.markers.png"), width=1200,height=1200,units="px")
-print(endothelial.markers)
-dev.off()
-
-# feature plot with mesenchymal cell markers
-mesenchymal.markers <- FeaturePlot(blood.harmony, features = mesenchymal.genes, pt.size = 0.5, ncol = 3) & 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
-png(paste0(harmony.path,"mesenchymal.markers.png"), width=1200,height=1200,units="px")
-print(mesenchymal.markers)
-dev.off()
-
-# feature plot with erythroid cell markers
-ery.markers <- FeaturePlot(blood.harmony, features = ery.genes, pt.size = 0.5, ncol = 2) & 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
-png(paste0(harmony.path,"ery.markers.png"), width=800,height=400,units="px")
-print(ery.markers)
-dev.off()
-
-# proliferating cells
-proliferation.markers <- FeaturePlot(blood.harmony, features = proliferation.genes, pt.size = 0.5, ncol = 2) & 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
-png(paste0(harmony.path,"proliferation.markers.png"), width=800,height=400,units="px")
-print(proliferation.markers)
-dev.off()
+### clustering ###
+# only for selected number of dims (for reasons of computational efficiency!)
+dims <- c(18,40,50)
+res <- seq(0.1,1.0,0.1)
+for(d in dims){
+  
+  # set path variable
+  dim.path <- paste0(harmony.path, "dim", d, "_annotation/")
+  
+  # visualization with UMAP
+  blood.harmony <- RunUMAP(blood.harmony, reduction = "harmony", dims=1:d, seed.use=1)
+  
+  # plot batch effect
+  batch.plot <- DimPlot(blood.harmony, reduction = "umap", group.by = "patient.ID", pt.size = 0.1)
+  
+  # create kNN graph
+  blood.harmony <- FindNeighbors(blood.harmony, reduction = "harmony", dims = 1:d)
+  
+  for (r in res) {
+    
+    blood.harmony <- FindClusters(blood.harmony, reduction = "harmony", resolution = r)
+    umap.plot <- DimPlot(blood.harmony, reduction = "umap", label = F, pt.size = 0.1)
+    
+    # create eval plot
+    eval.plot <- umap.plot + batch.plot
+    png(paste0(dim.path, "UMAP_dim", d, "_res", r, ".png"), width=1600, height=800, units="px")
+    print(eval.plot)
+    dev.off()
+    
+    # clustering at patient level
+    patient.clustering <- DimPlot(blood.harmony, group.by = "seurat_clusters", split.by = "patient.ID", pt.size = 0.1, ncol = 6)
+    png(paste0(dim.path,"UMAP_dim", d, "_res", r,".patient.clustering.png"), width=1800,height=1000,units="px")
+    print(patient.clustering)
+    dev.off()
+    
+  }
+}
