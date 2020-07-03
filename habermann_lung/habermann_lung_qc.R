4508eebc982e3602c137bff2246c67677a13903b
niklaslang <niklasjonathanlang@gmail.com>
2020-07-03 16:28:05 +0100

    performed QC: 44873 healthy cells, 119093 fibrotic cells

--- habermann_lung/habermann_lung_qc.R
+++ habermann_lung/habermann_lung_qc.R
@@ -455,7 +455,7 @@ png(paste0(lung.fibrotic.path,"QC.filtered.percent.mt.2.png"), width=1500,height
 print(percent.mt.plot2)
 dev.off()
 
-### save data: 119093 healthy cells ###
+### save data: 119093 fibrotic cells ###
 saveRDS(lung.fibrotic.filtered, paste0(lung.fibrotic.path, "habermann_lung_fibrotic.rds"))
 
 
